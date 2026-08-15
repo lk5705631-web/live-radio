05f33729d360b0c4c77ee414cb38922a90a1b23a
lk5705631-web <lk5705631@gmail.com>
2026-08-15 20:51:54 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ python3 -m http.server ${PORT:-10000} &
 while true; do
   for f in *.mp3; do
     echo "Now playing: $f"
-    ffmpeg -nostdin -re -i "$f" -c:a libmp3lame -b:a 96k -legacy_icecast 1 -content_type audio/mpeg -f mp3 "icecast://source:uTdAsJFD49@sapircast.caster.fm:13652/Drgtd"
+    ffmpeg -nostdin -re -i "$f" -c:a libmp3lame -b:a 96k -content_type audio/mpeg -legacy_icecast 1 -f mp3 "icecast://source:uTdAsJFD49@sapircast.caster.fm:13652/"
   done
 done
