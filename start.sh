72fc37080a2a3d89608cef18aeccc0f799206426
lk5705631-web <lk5705631@gmail.com>
2026-08-15 20:33:34 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python3 -m http.server ${PORT:-10000} &
+
+while true; do
+  for f in *.mp3; do
+    echo "Now playing: $f"
+    ffmpeg -nostdin -re -i "$f" -c:a libmp3lame -b:a 128k -content_type audio/mpeg -f mp3 "$STREAM_URL"
+  done
+done
